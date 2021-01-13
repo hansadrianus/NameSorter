ad1c81d23a5d23af2235e994e32a71a8e21bf5b1
Hans Adrianus <hans.adrianus@outlook.com>
2021-01-13 10:26:29 +0700

    Unit Test for each class

--- xUnitTest/SortNameTest.cs
+++ xUnitTest/SortNameTest.cs
@@ -1,4 +1,7 @@
 using NameSorterLibrary;
+using NameSorterLibrary.Export;
+using NameSorterLibrary.Name;
+using NameSorterLibrary.Sort;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,26 +15,82 @@ namespace xUnitTest
         [Fact]
         public void SortLastName()
         {
-            string data = "../../../File/unsorted-names-list.txt";
+            StandardMessage.Welcome();
+            StandardMessage.AddLine(1);
 
+            string data = SortName.InputFile();
+            IList<string> names = SortName.ReadData(data);
+            List<NameModel> people = new List<NameModel>();
+
+            StandardMessage.Enter();
+            StandardMessage.Clear();
+
+            SortName.Sort(names, people);
+
+            StandardMessage.AddLine(1);
+
+            ExportSortedNames.Export(people);
+        }
+
+        [Fact]
+        public IList<string> ReadData()
+        {
+            string data = "../../../File/unsorted-names-list.txt";
             IList<string> names = File.ReadLines(data).ToArray();
 
-            Console.WriteLine("Unsorted name list:");
-            foreach (var item in names)
+            return names;
+        }
+
+        [Fact]
+        public static void Sort()
+        {
+            string data = "../../../File/unsorted-names-list.txt";
+            IList<string> source = File.ReadLines(data).ToArray();
+
+            List<NameModel> result = new List<NameModel>();
+            SortProcessor sortProcessor = new SortProcessor();
+
+            foreach (var name in source)
             {
-                Console.WriteLine(item);
+                result.Add(sortProcessor.AppendData(name));
             }
-
-            StandardMessage.AddLine(3);
-
-            List<string> sortedNames = new List<string>(names);
-            sortedNames.Sort();
 
             Console.WriteLine("Sorted name list:");
-            foreach (var item in sortedNames)
+            foreach (var sort in result.OrderBy(q => q.IndexName).ThenBy(q => q.FullName))
             {
-                Console.WriteLine(item);
+                Console.WriteLine(sort.FullName);
             }
+        }
+
+        [Fact]
+        public static void Export()
+        {
+            string data = "../../../File/unsorted-names-list.txt";
+            IList<string> source = File.ReadLines(data).ToArray();
+
+            List<NameModel> result = new List<NameModel>();
+            SortProcessor sortProcessor = new SortProcessor();
+
+            foreach (var name in source)
+            {
+                result.Add(sortProcessor.AppendData(name));
+            }
+
+            Console.Write("Specify export directory: ");
+            string path = "../../../File";
+            FileStream stream = new FileStream(path + "/sorted-names-list.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter writer = new StreamWriter(stream);
+            TextWriter textWriter = Console.Out;
+
+            Console.SetOut(writer);
+            foreach (var sort in result.OrderBy(q => q.IndexName).ThenBy(q => q.FullName))
+            {
+                Console.WriteLine(sort.FullName);
+            }
+            Console.SetOut(textWriter);
+            writer.Close();
+            stream.Close();
+            Console.WriteLine("File exported with name sorted-names-list.txt");
         }
     }
 }
