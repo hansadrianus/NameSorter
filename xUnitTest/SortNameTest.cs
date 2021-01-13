29852b6341107a39f285e1be238cd29032514a9b
Hans Adrianus <hans.adrianus@outlook.com>
2021-01-13 10:30:33 +0700

    Update SortNameTest.cs

--- xUnitTest/SortNameTest.cs
+++ xUnitTest/SortNameTest.cs
@@ -13,26 +13,6 @@ namespace xUnitTest
     public class SortNameTest
     {
         [Fact]
-        public void SortLastName()
-        {
-            StandardMessage.Welcome();
-            StandardMessage.AddLine(1);
-
-            string data = SortName.InputFile();
-            IList<string> names = SortName.ReadData(data);
-            List<NameModel> people = new List<NameModel>();
-
-            StandardMessage.Enter();
-            StandardMessage.Clear();
-
-            SortName.Sort(names, people);
-
-            StandardMessage.AddLine(1);
-
-            ExportSortedNames.Export(people);
-        }
-
-        [Fact]
         public IList<string> ReadData()
         {
             string data = "../../../File/unsorted-names-list.txt";
